7faff62ccd95b17e27124eb3b6fab902269df821
Jayant Issar <jayantissar23@gmail.com>
2024-03-02 12:38:48 +0530

    removing all the vite given things

---
+++ backend/index.js
@@ -0,0 +1,6 @@
+// this file consists of the main backend things (this is the main file which will be executed)
+const express = require("express");
+const app = express()
+
+const cors = require('cors'); //calling cors
+
